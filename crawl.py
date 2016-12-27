d8d9f0d605c4f84362b58258747c7ec60c31108a
ray <eternalray123@gmail.com>
2016-12-27 12:36:19 +0900

    parsing page and discriminating image title and gif title

--- crawl.py
+++ crawl.py
@@ -1,13 +1,54 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from collections import deque
+
+
+def is_image(string):
+    image_pattern = "[0-9]+p|[0-9]+pic|과질|고화질"
+    image_regex = re.compile(image_pattern)
+    match = re.search(image_regex, string)
+    if match and not is_gif(string):
+        return True
+    else:
+        return False
+
+def is_gif(string):
+    gif_pattern = "gif|움짤"
+    gif_regex = re.compile(gif_pattern)
+    match = re.search(gif_regex ,string)
+    if match:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
 
     ''' to do : reqeusts exception handling
                 page parsing
     '''
-    response = requests.get("http://gall.dcinside.com/board/view/?id=twice&no=2555429&page=2&exception_mode=recommend")
+
+
+    queue = deque()
+
+    for i in range(1,1000):
+        page_url = "http://gall.dcinside.com/board/lists/?id=twice&page=" + str(i) + "&exception_mode=recommend"
+        response = requests.get(page_url)
+
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        ''' check whether the given url is image or gif by looking title
+            future work : tell whether it is image or gif by checking magic number of binary
+        '''
+
+        image_url = soup.find_all(attrs={ "class" : "icon_pic_b" }, string = is_image)
+        gif_url = soup.find_all(attrs={ "class" : "icon_pic_b" }, string = is_gif)
+
+        for j in range(len(image_url)):
+
+
+    #response = requests.get("")
+    #response = requests.get("http://gall.dcinside.com/board/view/?id=twice&no=2555429&page=2&exception_mode=recommend")
 
     ''' to do : BeautifulSoup exception handling '''
     soup = BeautifulSoup(response.text, 'lxml')
@@ -20,7 +61,9 @@ if __name__ == "__main__":
         '''to do : exception handling '''
         image_response = requests.get(image_src.attrs['src'], stream=True)
 
-        ''' file name should be determined '''
-        with open('twice' + str(i) + '.jpg', 'wb') as fd:
-            for chunk in image_response.iter_content(chunk_size=128):
+        ''' to do :file name should be determined
+                   check magic number and decide the extension of file
+        '''
+        with open('twice/twice' + str(i) + '.jpg', 'wb') as fd:
+            for chunk in image_response.iter_content(chunk_size=1024*1024):
                 fd.write(chunk)
