cc0e9bd707ff3a9f7587e76f5cb95fb1ee5e1404
ray <eternalray123@gmail.com>
2017-01-24 18:12:45 +0900

    minor code refactoring and implement image count status -> implement whole status processing and exception handling

--- crawl.py
+++ crawl.py
@@ -8,14 +8,20 @@
                 modularizing -> done???
                 file deduplication
                 machine learning
+                implement atomicity of crawling binary
                 etc...
 '''
 
 from twice_crawler import *
 
 if __name__ == "__main__":
+    try:
+        fd = open("crawl_status.txt","r")
+        fd.seek(14)
+        count_image = int(fd.read()) # get image count from crawl_status.txt
 
-    count_image = 0
+    except FileNotFoundError:
+        count_image = 0 # if it is the first time doing crawling, set count_image to 0
 
     crawler = Crawler(count_image)
     crawler.crawl()
--- twice_crawler/__init__.py
+++ twice_crawler/__init__.py
@@ -1,4 +1,4 @@
-
+import sys
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -10,39 +10,57 @@ class Crawler:
     def __init__(self,count_image):
         self.count_image = count_image
 
+    def _isImage(self,string):
+        image_pattern = "[0-9]+p|[0-9]+pic|과질|고화질|!프리뷰|!플뷰|gif|움짤|퇴근길|출근길"
+        image_regex = re.compile(image_pattern)
+        match = re.search(image_regex, string)
+        return match
+
+    def _crawl(sel,count,url_tag):
+        for i in range(len(url_tag)):
+            print("crawling " + url_tag[i].string)
+            article_url = "http://gall.dcinside.com/" + url_tag[i].attrs['href']
+            response = requests.get(article_url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            tag = soup.find_all(attrs={ "app_paragraph" : re.compile("^Dc_App_Img_[0-9]+$"),"app_editorno" : re.compile("^[0-9]+$") })
+            save_path = ""
+
+            for j in range(len(tag)):
+                src = tag[j].find('img')
+                image_response = requests.get(src.attrs['src'],stream=True)
+                save_path = "twice_img/twice" + str(count + 1)
+                with open(save_path, "wb") as fd:
+                    for chunk in image_response.iter_content(chunk_size=1024*1024):
+                        fd.write(chunk)
+                count += 1
+        return count
     def crawl(self):
+        start_article_number = None
+        end_article_number = None
+        num_of_page = sys.argv[1] # get command line argument for the number of pages to crawl, future work : find better way to do this?
+        for i in range(1,int(num_of_page) + 1):
+            ''' parsing page by page '''
 
-        def _is_image(string):
-            image_pattern = "[0-9]+p|[0-9]+pic|과질|고화질|!프리뷰|!플뷰|gif|움짤|퇴근길|출근길"
-            image_regex = re.compile(image_pattern)
-            match = re.search(image_regex, string)
-            return match
-
-        def _crawl(count, url_tag):
-
-            for i in range(len(url_tag)):
-                print("crawling " + url_tag[i].string)
-                article_url = "http://gall.dcinside.com/" + url_tag[i].attrs['href']
-                response = requests.get(article_url)
-
-                soup = BeautifulSoup(response.text, 'lxml')
-                tag = soup.find_all(attrs={ "app_paragraph" : re.compile("^Dc_App_Img_[0-9]+$"), "app_editorno" : re.compile("^[0-9]+$") })
-                save_path = ""
-
-                for j in range(len(tag)):
-                    src = tag[j].find('img')
-                    image_response = requests.get(src.attrs['src'], stream=True)
-                    save_path = "twice_img/twice" + str(count + 1)
-                    with open(save_path, "wb") as fd:
-                        for chunk in image_response.iter_content(chunk_size=1024*1024):
-                            fd.write(chunk)
-                    count += 1
-
-
-        for i in range(1,1000):
             page_url = "http://gall.dcinside.com/board/lists/?id=twice&page=" + str(i) + "&exception_mode=recommend"
             response = requests.get(page_url)
-            soup = BeautifulSoup(response.text, 'lxml')
-            image_url_tag = soup.find_all(attrs={ "class" : "icon_pic_b" }, string=_is_image)
+            soup = BeautifulSoup(response.text,'lxml')
 
-            _crawl(self.count_image, image_url_tag)
+            image_url_tag = soup.find_all(attrs={ "class" : "icon_pic_b" },string=self._isImage)
+
+            ''' if image_url_tag is None or empty, continue the loop '''
+
+            if not image_url_tag:
+                continue
+
+            ''' assign crawling start point '''
+
+            if start_article_number is None:
+                start_article_number = image_url_tag[0].parent.find_previous_sibling("td").string
+
+            self.count_image = self._crawl(self.count_image,image_url_tag)
+
+        ''' assign crawling end point and log crawl status'''
+
+        end_article_number = image_url_tag[len(image_url_tag)-1].parent.find_previous_sibling("td").string
+        with open("crawl_status.txt","w") as fd:
+            fd.write("count_image = " + str(self.count_image)) # log image count
