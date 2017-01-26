f8932a7347ed17b223c79efe83b5023c8621992b
ray <eternalray123@gmail.com>
2017-01-26 21:54:06 +0900

    minor refactoring and saving all the crawling status -> implementing exception handler

--- crawl.py
+++ crawl.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     try:
         fd = open("crawl_status.txt","r")
         fd.seek(14)
-        count_image = int(fd.read()) # get image count from crawl_status.txt
+        count_image = int(fd.readline()) # get image count from crawl_status.txt
 
     except FileNotFoundError:
         count_image = 0 # if it is the first time doing crawling, set count_image to 0
--- twice_crawler/__init__.py
+++ twice_crawler/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import requests
+import datetime
 from bs4 import BeautifulSoup
 from collections import deque
 
@@ -11,7 +12,7 @@ class Crawler:
         self.count_image = count_image
 
     def _isImage(self,string):
-        image_pattern = "[0-9]+p|[0-9]+pic|과질|고화질|!프리뷰|!플뷰|gif|움짤|퇴근길|출근길"
+        image_pattern = "[0-9]+p|[0-9]+pic|[0-9]+P|[0-9]+PIC|과질|고화질|!프리뷰|!플뷰|gif|움짤|퇴근길|출근길|by|BY|By"
         image_regex = re.compile(image_pattern)
         match = re.search(image_regex, string)
         return match
@@ -23,8 +24,15 @@ class Crawler:
             response = requests.get(article_url)
             soup = BeautifulSoup(response.text, 'lxml')
             tag = soup.find_all(attrs={ "app_paragraph" : re.compile("^Dc_App_Img_[0-9]+$"),"app_editorno" : re.compile("^[0-9]+$") })
-            save_path = ""
 
+            ''' I dont know why this happens but rarely, without standard app_paragraph and app_editorno, only class name "txc-image" comes with img tag '''
+
+            if not tag:
+                tag = soup.find_all(attrs={ "class" : "txc-image" })
+                for j in range(len(tag)):
+                    tag[j] = tag[j].parent # we have to modify this because the DOM tree is totally different from normal one
+
+            save_path = ""
             for j in range(len(tag)):
                 src = tag[j].find('img')
                 image_response = requests.get(src.attrs['src'],stream=True)
@@ -47,7 +55,7 @@ class Crawler:
 
             image_url_tag = soup.find_all(attrs={ "class" : "icon_pic_b" },string=self._isImage)
 
-            ''' if image_url_tag is None or empty, continue the loop '''
+            ''' if image_url_tag is None or empty, continue to crawl next page '''
 
             if not image_url_tag:
                 continue
@@ -62,5 +70,9 @@ class Crawler:
         ''' assign crawling end point and log crawl status'''
 
         end_article_number = image_url_tag[len(image_url_tag)-1].parent.find_previous_sibling("td").string
+
         with open("crawl_status.txt","w") as fd:
-            fd.write("count_image = " + str(self.count_image)) # log image count
+            fd.write("count_image = " + str(self.count_image) + "\n") # log image count
+            fd.write("start point = " + str(start_article_number) + "\n") # log starting point with article number
+            fd.write("end point = " + str(end_article_number)+ "\n") # log end point with article number`
+            fd.write("crawling done in " + str(datetime.datetime.now()) + "\n") # timestamp
